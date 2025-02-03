c2d2adc7942e66c983c5b6f4e8d5f07745678b80
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2025-02-03 12:01:12 -0500

    Show types entered!

--- TenShowsList/ContentView.swift
+++ TenShowsList/ContentView.swift
@@ -15,6 +15,21 @@ struct Show: Identifiable {
 }
 
 struct ContentView: View {
+    let shows = [
+        Show(name: "Avatar: The Last Airbender", description: "An epic story of balance and bending.", imageName: "avatar"),
+        Show(name: "Brooklyn 99", description: "A comedic take on precinct life.", imageName: "brooklyn99"),
+        Show(name: "A Series of Unfortunate Events", description: "A dark and quirky tale of three siblings.", imageName: "asoue"),
+        Show(name: "Gravity Falls", description: "Mystery and adventure in a small town.", imageName: "gravityfalls"),
+        Show(name: "Sherlock", description: "Modernized detective brilliance.", imageName: "sherlock"),
+        Show(name: "Queen's Gambit", description: "A journey of chess and self-discovery.", imageName: "queensgambit")
+    ]
+    
+    
+    
+    
+    
+    
+    View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
