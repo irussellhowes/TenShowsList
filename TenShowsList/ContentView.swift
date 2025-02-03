0260bbf0b6ac614b8d46c74f79656a25f84eaed1
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2025-02-03 11:45:54 -0500

    Initial Commit

---
+++ TenShowsList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TenShowsList
+//
+//  Created by Isla Russell-Howes on 2025-02-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TenShowsList/TenShowsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenShowsListApp.swift
+//  TenShowsList
+//
+//  Created by Isla Russell-Howes on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct TenShowsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
